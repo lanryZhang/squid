2db381330f23b592449aec7918c7e07039239b69
lanryzhang <hookyzlr@163.com>
2016-03-30 18:00:33 +0800

    del

--- src/main/java/com/ifeng/configuration/IConfig.java
+++
@@ -1,8 +0,0 @@
-package com.ifeng.configuration;
-
-/**
- * Created by zhanglr on 2016/3/29.
- */
-public interface IConfig {
-    void initFile();
-}
--- src/main/java/com/ifeng/mongo/IDecode.java
+++
@@ -1,8 +0,0 @@
-package com.ifeng.mongo;
-
-/**
- * Created by zhanglr on 2016/2/24.
- */
-public interface IDecode {
-    void decode(DataLoader loader);
-}
